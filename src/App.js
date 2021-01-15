b1b8fccab56f43f8861d64467db8059d3d014dc3
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-14 23:39:02 -0500

    added main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {Table, TableCell, TableRow} from "./components"
 
 function App() {
   return (
     <div className="App">
-      
+      <Table/>
+      <TableCell/>
+      <TableRow/>
     </div>
   );
 }
---
+++ src/components/TableRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TableRow() {
+    return (
+        <div>
+            <h1>Table Row</h1>
+        </div>
+    )
+}
